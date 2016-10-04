98fa6bd0cd1f69ae5ceeb79d5a8e1faf5ac21d8a
Mark <markverleene@hotmail.com>
2016-10-04 15:26:47 +0200

    code voor object nr base64

--- ictProject3/Form1.cs
+++ ictProject3/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +22,20 @@ namespace ictProject3
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        public static string SerializeBase64(object o)
+        {
+            // Serialize to a base 64 string
+            byte[] bytes;
+            long length = 0;
+            MemoryStream ws = new MemoryStream();
+            BinaryFormatter sf = new BinaryFormatter();
+            sf.Serialize(ws, o);
+            length = ws.Length;
+            bytes = ws.GetBuffer();
+            string encodedData = bytes.Length + ":" + Convert.ToBase64String(bytes, 0, bytes.Length, Base64FormattingOptions.None);
+            return encodedData;
         }
     }
 }
