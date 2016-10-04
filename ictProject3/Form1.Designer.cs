b1a4d53a69efa01d8de4a36a7155f7241d3c0bff
Mark <markverleene@hotmail.com>
2016-10-04 14:59:37 +0200

    sprint 1 GUI

---
+++ ictProject3/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ictProject3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownloadFile = new System.Windows.Forms.Button();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.btnUpdateList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDownloadFile
+            // 
+            this.btnDownloadFile.Location = new System.Drawing.Point(228, 12);
+            this.btnDownloadFile.Name = "btnDownloadFile";
+            this.btnDownloadFile.Size = new System.Drawing.Size(99, 42);
+            this.btnDownloadFile.TabIndex = 0;
+            this.btnDownloadFile.Text = "Download selected file";
+            this.btnDownloadFile.UseVisualStyleBackColor = true;
+            this.btnDownloadFile.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.Location = new System.Drawing.Point(12, 12);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(210, 147);
+            this.lstFiles.TabIndex = 1;
+            // 
+            // btnUpdateList
+            // 
+            this.btnUpdateList.Location = new System.Drawing.Point(229, 61);
+            this.btnUpdateList.Name = "btnUpdateList";
+            this.btnUpdateList.Size = new System.Drawing.Size(98, 23);
+            this.btnUpdateList.TabIndex = 2;
+            this.btnUpdateList.Text = "Update list";
+            this.btnUpdateList.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 313);
+            this.Controls.Add(this.btnUpdateList);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.btnDownloadFile);
+            this.Name = "Form1";
+            this.Text = "ictProject3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownloadFile;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.Button btnUpdateList;
+    }
+}
+
