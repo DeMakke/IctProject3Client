4deb4be2d63a05792e9807dda306f02d5157b4ba
Maxim Bergmans <bergmansmaxim@hotmail.com>
2016-10-04 15:57:02 +0200

    toevoegen + coderen klasse Item, code in form voor listbox te vullen

--- ictProject3/Form1.cs
+++ ictProject3/Form1.cs
@@ -19,10 +19,7 @@ namespace ictProject3
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
-        }
 
         public static string SerializeBase64(object o)
         {
@@ -40,7 +37,26 @@ namespace ictProject3
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            List<Item> itemList = Item.GetItems();
+            lstFiles.Items.Clear();
 
+            foreach (Item item in itemList)
+            {
+                lstFiles.Items.Add((Convert.ToString(item.Id)).PadRight(15) + (Convert.ToString(item.Naam)));
+            }
+        }
+
+        private void btnDownloadFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdateList_Click(object sender, EventArgs e)
+        {
+            //kijk welke geselecteerd is
+            //maak een object van het geselecteerde item
+
+            var selectedFromListBox = lstFiles.SelectedItem;
         }
     }
 }
