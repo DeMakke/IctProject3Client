1e6f6e6787bc6a9909f78636198b145486dcddba
Frederik <frederik.vanstraelen@student.ucll.be>
2016-10-09 18:04:45 +0200

    Created Class: Base64Code.cs and integrated 2 functions from Form1.cs

---
+++ ictProject3/Base64Code.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ictProject3
+{
+    public class Base64Code
+    {
+        public static string SerializeBase64(object o)
+        {
+            // Serialize to a base 64 string
+            byte[] bytes;
+            long length = 0;
+            MemoryStream ws = new MemoryStream();
+            BinaryFormatter sf = new BinaryFormatter();
+            sf.Serialize(ws, o);
+            length = ws.Length;
+            bytes = ws.GetBuffer();
+            string encodedData = bytes.Length + ":" + Convert.ToBase64String(bytes, 0, bytes.Length, Base64FormattingOptions.None);
+            return encodedData;
+        }
+
+        private Tuple<byte[], string> DeSerializeBase64(Data o)
+        {
+            string naam = "";
+
+            byte[] bitarray = Convert.FromBase64String(o.base64);
+
+            return Tuple.Create(bitarray, naam);
+        }
+    }
+}
--- ictProject3/Form1.cs
+++ ictProject3/Form1.cs
@@ -28,30 +28,6 @@ namespace ictProject3
 
         }
 
-
-        public static string SerializeBase64(object o)
-        {
-            // Serialize to a base 64 string
-            byte[] bytes;
-            long length = 0;
-            MemoryStream ws = new MemoryStream();
-            BinaryFormatter sf = new BinaryFormatter();
-            sf.Serialize(ws, o);
-            length = ws.Length;
-            bytes = ws.GetBuffer();
-            string encodedData = bytes.Length + ":" + Convert.ToBase64String(bytes, 0, bytes.Length, Base64FormattingOptions.None);
-            return encodedData;
-        }
-
-        private Tuple<byte[], string> DeSerializeBase64(Data o)
-        {
-            string naam = "";
-
-            byte[] bitarray = Convert.FromBase64String(o.base64);
-
-            return Tuple.Create(bitarray, naam);
-        }
-
         private void ReportProgress(int value)
         {
             //progressreport
