9a1973043d10cad32c37ac55ff1e16aaeb010c24
JanLaptop\Jan <j.youlton@gmail.com>
2016-12-29 14:55:52 +0100

    change login geupdate

--- ictProject3/GebruikerWijzigen.Designer.cs
+++ ictProject3/GebruikerWijzigen.Designer.cs
@@ -34,11 +34,15 @@
             this.userNameLabel = new System.Windows.Forms.Label();
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RepeatPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.oldPasswordTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnAnnuleren
             // 
-            this.btnAnnuleren.Location = new System.Drawing.Point(216, 82);
+            this.btnAnnuleren.Location = new System.Drawing.Point(226, 120);
             this.btnAnnuleren.Margin = new System.Windows.Forms.Padding(2);
             this.btnAnnuleren.Name = "btnAnnuleren";
             this.btnAnnuleren.Size = new System.Drawing.Size(56, 27);
@@ -49,38 +53,38 @@
             // 
             // btnOpslaan
             // 
-            this.btnOpslaan.Location = new System.Drawing.Point(116, 82);
+            this.btnOpslaan.Location = new System.Drawing.Point(126, 120);
             this.btnOpslaan.Margin = new System.Windows.Forms.Padding(2);
             this.btnOpslaan.Name = "btnOpslaan";
             this.btnOpslaan.Size = new System.Drawing.Size(59, 27);
             this.btnOpslaan.TabIndex = 10;
-            this.btnOpslaan.Text = "Opslaan";
+            this.btnOpslaan.Text = "Save";
             this.btnOpslaan.UseVisualStyleBackColor = true;
             this.btnOpslaan.Click += new System.EventHandler(this.btnOpslaan_Click);
             // 
             // passwordLabel
             // 
             this.passwordLabel.AutoSize = true;
-            this.passwordLabel.Location = new System.Drawing.Point(31, 49);
+            this.passwordLabel.Location = new System.Drawing.Point(18, 71);
             this.passwordLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.passwordLabel.Name = "passwordLabel";
-            this.passwordLabel.Size = new System.Drawing.Size(59, 13);
+            this.passwordLabel.Size = new System.Drawing.Size(84, 13);
             this.passwordLabel.TabIndex = 9;
-            this.passwordLabel.Text = "Password :";
+            this.passwordLabel.Text = "New Password :";
             // 
             // userNameLabel
             // 
             this.userNameLabel.AutoSize = true;
-            this.userNameLabel.Location = new System.Drawing.Point(24, 17);
+            this.userNameLabel.Location = new System.Drawing.Point(11, 23);
             this.userNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.userNameLabel.Name = "userNameLabel";
-            this.userNameLabel.Size = new System.Drawing.Size(66, 13);
+            this.userNameLabel.Size = new System.Drawing.Size(91, 13);
             this.userNameLabel.TabIndex = 8;
-            this.userNameLabel.Text = "User Name :";
+            this.userNameLabel.Text = "New User Name :";
             // 
             // passwordTextBox
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(94, 47);
+            this.passwordTextBox.Location = new System.Drawing.Point(126, 68);
             this.passwordTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.passwordTextBox.Name = "passwordTextBox";
             this.passwordTextBox.Size = new System.Drawing.Size(213, 20);
@@ -89,17 +93,59 @@
             // 
             // userNameTextBox
             // 
-            this.userNameTextBox.Location = new System.Drawing.Point(94, 14);
+            this.userNameTextBox.Location = new System.Drawing.Point(126, 20);
             this.userNameTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.userNameTextBox.Name = "userNameTextBox";
             this.userNameTextBox.Size = new System.Drawing.Size(213, 20);
             this.userNameTextBox.TabIndex = 6;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Repeat Password :";
+            // 
+            // RepeatPasswordTextBox
+            // 
+            this.RepeatPasswordTextBox.Location = new System.Drawing.Point(126, 92);
+            this.RepeatPasswordTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.RepeatPasswordTextBox.Name = "RepeatPasswordTextBox";
+            this.RepeatPasswordTextBox.Size = new System.Drawing.Size(213, 20);
+            this.RepeatPasswordTextBox.TabIndex = 12;
+            this.RepeatPasswordTextBox.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Old Password :";
+            // 
+            // oldPasswordTextBox
+            // 
+            this.oldPasswordTextBox.Location = new System.Drawing.Point(126, 44);
+            this.oldPasswordTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.oldPasswordTextBox.Name = "oldPasswordTextBox";
+            this.oldPasswordTextBox.Size = new System.Drawing.Size(213, 20);
+            this.oldPasswordTextBox.TabIndex = 14;
+            this.oldPasswordTextBox.UseSystemPasswordChar = true;
+            // 
             // GebruikerWijzigen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 123);
+            this.ClientSize = new System.Drawing.Size(350, 158);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.oldPasswordTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RepeatPasswordTextBox);
             this.Controls.Add(this.btnAnnuleren);
             this.Controls.Add(this.btnOpslaan);
             this.Controls.Add(this.passwordLabel);
@@ -121,5 +167,9 @@
         private System.Windows.Forms.Label userNameLabel;
         private System.Windows.Forms.TextBox passwordTextBox;
         private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox RepeatPasswordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox oldPasswordTextBox;
     }
 }
